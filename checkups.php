5d1564933470561e562a74b732a643a1c5b04665
Yaoting Li <87930183+REN2O@users.noreply.github.com>
2021-10-10 22:03:26 +1100

    Add files via upload

---
+++ checkups.php
@@ -0,0 +1,54 @@
+<?php
+	include_once 'header.php';
+	include_once 'dbh.inc.php';
+?>
+
+<title>CHECK-UPS</title>
+<h1>CHECK-UPS</h1> 
+	
+<body>
+
+	
+
+		<style>
+		th, td {
+ 		 border-bottom: 1px solid #ddd;
+		}
+	</style>
+
+
+<h2 style="text-align:center;">Check-ups Table</h2>
+<table style="width:50%" align="center">
+	
+		<tr>	
+			<th>Pet ID</th>		
+			<th>Vet Username</th>
+			<th>Vet Fullname</th>
+			<th>Check-ups</th>
+			<th>Check-ups Notes</th>
+
+		</tr>
+					<?php
+						if (isset($_SESSION["username"])) {
+							$uid = $_SESSION['username'];
+						$query = "SELECT * FROM `checkups` where Account_username = '$uid'";
+						$query_run = mysqli_query($conn, $query);
+
+						while ($row = mysqli_fetch_array($query_run)) {
+						?>
+						<tr style="text-align:center">
+							<td><?php echo $row['Pet_id']; ?></td>
+							<td><?php echo $row['Vet_username']; ?></td>
+							<td><?php echo $row['Vet_fullname']; ?></td>
+							<td><?php echo $row['Checkups_type']; ?></td>
+							<td><?php echo $row['Checkups_notes']; ?></td>
+						</tr>
+						<?php
+						}
+						
+						}
+					?>
+						
+	</table>
+
+</body>
