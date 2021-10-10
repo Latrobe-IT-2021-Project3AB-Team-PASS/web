4cc32df699acbbd6410de0047e73d879dcc00cba
Yaoting Li <87930183+REN2O@users.noreply.github.com>
2021-10-10 22:18:00 +1100

    Update checkups.php

--- checkups.php
+++ checkups.php
@@ -3,8 +3,9 @@
 	include_once 'dbh.inc.php';
 ?>
 
-<title>CHECK-UPS</title>
-<h1>CHECK-UPS</h1> 
+<title>CHECK-UPS & PARASITE PREVENTION</title>
+<h1>CHECK-UPS &
+<br>PREVENTION</h1> 
 	
 <body>
 
@@ -16,9 +17,10 @@
 		}
 	</style>
 
-
-<h2 style="text-align:center;">Check-ups Table</h2>
-<table style="width:50%" align="center">
+<section style="text-align:center">
+	<h2>Check-ups Table</h2>
+	
+	<table style="width:50%" align="center">
 	
 		<tr>	
 			<th>Pet ID</th>		
@@ -50,5 +52,37 @@
 					?>
 						
 	</table>
+</section>
+	<section style="text-align:center">
+		<h2>Parasite Prevention Table</h2>
+	
+	<table style="width:50%" align="center">
+	
+		<tr>	
+			<th>Pet ID</th>		
+			<th>Date</th>
+			<th>Prevention Product</th>
+			<th>Fuequency</th>
+		</tr>
+					<?php
+						if (isset($_SESSION["username"])) {
+							$uid = $_SESSION['username'];
+						$query = "SELECT * FROM `ParasitePrevention` where Account_username = '$uid'";
+						$query_run = mysqli_query($conn, $query);
 
+						while ($row = mysqli_fetch_array($query_run)) {
+						?>
+						<tr style="text-align:center">
+							<td><?php echo $row['Pet_id']; ?></td>
+							<td><?php echo $row['PP_date']; ?></td>
+							<td><?php echo $row['PP_product']; ?></td>
+							<td><?php echo $row['PP_fuequency']; ?></td>
+						</tr>
+						<?php
+						}
+						
+						}
+					?>
+						
+	</table>
 </body>
