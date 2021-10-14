19da2c98172db08b06988c78fe182123f6ae1008
Yaoting Li <87930183+REN2O@users.noreply.github.com>
2021-10-14 23:54:45 +1100

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -101,54 +101,23 @@ function emptyInputLogin($username, $pwd){
 }
 
 function loginUser($conn, $username, $pwd){
+	$uidExists = uidExists($conn, $username);
 
-        $sql = "SELECT Account_username, Account_password FROM Account WHERE username = ?";
+	if ($uidExists === false) {
+		header("location: signUp.php?error=wronglogin");
+		exit();
+	}
 
-        
-        if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($pwd, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            session_start();
-							$_SESSION["username"] = $username;
-                            header("location: home.php");
-                        } else{
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
-                        }
-                    }
-                } else{
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Invalid username or password.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
+	$pwdHashed = $uidExists["usersPwd"];
+	$checkPwd = password_verify($pwd, $pwdHashed);
+	if ($checkPwd === false) {
+		header("location: logIn.php?error=wronglogin");
+	}
+	else if ($checkPwd === true) {
+		session_start();
+		$_SESSION["userid"] = $uidExists["usersId"];
+		$_SESSION["useruid"] = $uidExists["usersUid"];
+		header("location: logIn.php?error=wronglogin");
+		exit();
+	}
 }
-
