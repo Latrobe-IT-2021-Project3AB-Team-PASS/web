9e76a12163d267f7f267759857083c6fbe1c90cc
Yaoting Li <87930183+REN2O@users.noreply.github.com>
2021-10-14 23:57:42 +1100

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -121,3 +121,54 @@ function loginUser($conn, $username, $pwd){
 		exit();
 	}
 }
+	
+
+        $sql = "SELECT Account_username, Account_password FROM account WHERE username = ?";
+
+        
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($pwd, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            session_start();
+							$_SESSION["username"] = $username;
+                            header("location: home.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+}
